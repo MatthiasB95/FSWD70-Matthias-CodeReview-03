6b961a93d18f7a7156676b7743199ea87d44237e
Matthias Bruckbeck <MBruckbeck@gmx.at>
2019-05-25 12:56:50 +0200

    added jasmine

---
+++ jasmine/spec/testspec.js
@@ -0,0 +1,29 @@
+describe("calculateInsurance", function(){
+
+       // what it should do
+       it("should Return res",function(){
+               //expect something
+               expect(calculateInsurance(25, 200)).toEqual(850);
+       });
+
+});
+
+describe("calculateInsurance", function(){
+
+       // what it should do
+       it("should Return res2",function(){
+               //expect something
+               expect(calculateInsurance(30, 300)).toEqual(1050);
+       });
+
+});
+
+describe("calculateInsurance", function(){
+
+       // what it should do
+       it("should Return res3",function(){
+               //expect something
+               expect(calculateInsurance(10, 10)).toEqual(150);
+       });
+
+});
---
+++ jasmine/src/codereview.js
@@ -0,0 +1,26 @@
+function drop(){
+	document.getElementById("myDropdown").classList.toggle("show");
+}
+
+function calculateInsurance(power, age){
+	var name = document.getElementById("name").value;
+	var age = document.getElementById("age").value;
+	var power = document.getElementById("power").value; 
+
+	var e = document.getElementById("dropcalc").value;
+	
+	if (e == 0) {
+		var res = parseInt(power) * 100 / parseInt(age) + 50;
+		document.getElementById("result").innerHTML = name + ", your insurance costs " + res +"€";
+	} else if (e == 1) {
+		var res2 = parseInt(power) * 120 / parseInt(age) + 100;
+		document.getElementById("result").innerHTML = name + ", your insurance costs " + res2 +"€";
+	}else if (e == 2) {
+		var res3 = Math.floor(parseInt(power) * 150 / (parseInt(age)+3) + 50);
+		document.getElementById("result").innerHTML = name + ", your insurance costs " + res3 +"€";
+	}
+	return res;
+	return res2;
+	return res3;
+}
+
